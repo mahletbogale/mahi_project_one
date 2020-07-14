bc7dfd906bc45db1e1a845b350868cf650477887
mahi <mahlet.luluu@gmail.com>
2020-07-14 19:22:40 -0400

    grab an element

--- flashcard.js
+++ flashcard.js
@@ -6,4 +6,16 @@ const playBtn = document.querySelector('.button_container');
 
 playBtn.addEventListener('click',getPicture);
 
+// grabing image area and store it to new variable
+// grabing every button click and store it to new variable
+// grabing word area and assign it to new variable
+// grabing card(means DIV which holds both image and word DIVs) area and assign it to new variable
+
+    
+let imageArea = document.getElementById('cardImg');
+let  eventBtn= event.target.id;
+let wordA =document.querySelector('.word');
+let cardA =document.querySelector('.word_card_container');
+
+
 // 
